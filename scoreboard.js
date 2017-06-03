3df21b0b35483744a9159d464ed31d7998d3f776
Andy Pan <andy0130tw@yahoo.com.tw>
2017-06-04 03:04:51 +0800

    update; some refactoring

--- scoreboard.js
+++ scoreboard.js
@@ -1,3 +1,4 @@
+// for sorting two values
 jQuery.extend(jQuery.fn.dataTableExt.oSort, {
   "dual-asc": function(x, y) {
     if (!x) return -1;
@@ -19,7 +20,8 @@ jQuery.extend(jQuery.fn.dataTableExt.oSort, {
 var columns = [
   { title: '#', defaultContent: '', className: 'cell-rank text-right', orderable: false, searchable: false },
   { title: 'User', data: 'username', className: 'cell-user' },
-  { title: 'Trials', data: 'stat', className: 'cell-trial text-right hidden-xs hidden-sm', type: 'dual', orderSequence: [ 'desc', 'asc' ],
+  { title: 'Trials', data: 'stat', className: 'cell-trial text-right hidden-xs hidden-sm', type: 'dual',
+    orderSequence: [ 'desc', 'asc' ],
     render: function(stat, type) {
       if (type == 'sort') {
         return stat ? [ stat.ac_count, -stat.trial_ttl ] : '';
@@ -28,7 +30,7 @@ var columns = [
       return stat.ac_count + ' / ' + stat.trial_ttl;
     }
   },
-  { title: 'Score', data: 'score', className: 'cell-score text-right' },
+  { title: 'Score', data: 'score', className: 'cell-score text-right', orderSequence: [ 'desc', 'asc' ] },
 ];
 
 var probRenderFunc = function(obj, type) {
@@ -43,38 +45,44 @@ var probRenderFunc = function(obj, type) {
 
 var probMarkFunc = function(td, cellData, rowData, row, col) {
   if (cellData.verdict == 'AC') {
-    $(td).addClass('cell-ac');
+    td.classList.add('cell-ac');
   } else if (cellData.verdict == 'WA') {
-    $(td).addClass('cell-wa');
+    td.classList.add('cell-wa');
   } else if (cellData.verdict == 'TLE') {
-    $(td).addClass('cell-tle');
+    td.classList.add('cell-tle');
   }
 };
 
-problems.forEach(function(p) {
-  p.title = '<div class="text-prob-title">' + p.title + '</div>';
-  if (p.user_accepted) {
-    p.title += '<div class="text-prob-stat">' + p.user_accepted + '/' + p.user_tried + '</div>';
-  }
-  p.title += '<div class="text-prob-team-id">' + (p.pid - 70000) + '</div>';
+var problemTargets = [];
+problems.forEach(function(p, i) {
+  var titleArr = [
+    '<div class="text-prob-title">' + p.title + '</div>',
+    p.user_accepted ?
+      '<div class="text-prob-stat">' + p.user_accepted + '/' + p.user_tried + '</div>' : '',
+    '<div class="text-prob-team-id">' + (p.pid - 70000) + '</div>'
+  ];
+  p.title = titleArr.join('');
   p.data = 'result_' + p.pid;
-  p.className = 'cell-prob cell-brob-' + p.pid;
-  p.defaultContent = '<div class="text-status-na">&mdash;</div>';
-  p.render = probRenderFunc;
-  p.type = 'dual';
-  p.orderSequence = [ 'desc' ];
-  p.width = '6rem';
-  p.createdCell = probMarkFunc;
+  p.className = 'cell-prob cell-prob-' + p.pid;
   columns.push(p);
+  problemTargets.push(i + 4);  // problem starts at 4th column
 });
 
 var t = $('#mainTable').DataTable({
   data: data,
   columns: columns,
   columnDefs: [
-
+    {
+      targets: problemTargets,
+      defaultContent: '<div class="text-status-na">&mdash;</div>',
+      render: probRenderFunc,
+      createdCell: probMarkFunc,
+      orderSequence: [ 'desc' ],
+      width: '6rem',
+      type: 'dual',
+      searchable: false
+    }
   ],
-  // paging: false,
   processing: true,
   lengthMenu: [ 16, 25, 32, 50, 100, 200 ],
   order: [[ 3, 'desc' ]],
@@ -83,10 +91,9 @@ var t = $('#mainTable').DataTable({
   fixedColumns: {
     leftColumns: 4
   },
-  // deferRender: true,
   colReorder: {
     fixedColumnsLeft: 4
-  }
+  },
 });
 
 t.on('order.dt search.dt', function () {
@@ -96,3 +103,11 @@ t.on('order.dt search.dt', function () {
       cell.innerHTML = i + 1;
     });
 }).draw();
+
+(function(b,c){var $=b.jQuery||b.Cowboy||(b.Cowboy={}),a;$.throttle=a=function(e,f,j,i){var h,d=0;if(typeof f!=="boolean"){i=j;j=f;f=c}function g(){var o=this,m=+new Date()-d,n=arguments;function l(){d=+new Date();j.apply(o,n)}function k(){h=c}if(i&&!h){l()}h&&clearTimeout(h);if(i===c&&m>e){l()}else{if(f!==true){h=setTimeout(i?k:l,i===c?e-m:e)}}}if($.guid){g.guid=j.guid=j.guid||$.guid++}return g};$.debounce=function(d,e,f){return f===c?a(d,e,false):a(d,f,e!==false)}})(this);
+
+$(window)
+  .off('resize')
+  .on('resize', $.debounce(200, function() {
+    t.columns.adjust();
+  }));
